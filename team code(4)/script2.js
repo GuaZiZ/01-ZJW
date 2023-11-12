fa203991021377a1296215855d1909e8ed94c8db
GuaZiZ <1482821758@qq.com>
2023-11-12 19:27:18 +0800

    first commit

---
+++ team code(4)/script2.js
@@ -0,0 +1,37 @@
+var title=document.getElementsByClassName('title');
+        var para=document.getElementsByClassName('para');
+
+    for(i0=0;i0<title.length;i0++) {
+        title[i0].style.animationDelay = (i0*0.1)+"s";
+    }
+
+    for(i1=0;i1<para.length;i1++) {
+        para[i1].style.animationDelay = (i1*0.1)+"s";
+    }
+
+
+
+    setInterval(function(){
+    $.ajax({
+        url: '/get_json_data',
+        type: 'POST',
+        dataType: 'json',
+        data: {},
+    })
+    .done(function(data) {
+        if(data.status==1){
+            let str = '';
+            let tem_data = data.data;
+            for(let i=0; i<tem_data.length; i++){
+                str += '<div>';
+                str += 'object：'+ tem_data[i]['object'] + '；'+'count：' + tem_data[i]['count'];
+                str += '</div>';
+            }
+            // $('#json_data').html(JSON.stringify(data.data));
+             $('#json_data').html(str);
+            console.log(data);
+        }else{
+        alert(data.msg);
+        }
+    })
+}, 500);
